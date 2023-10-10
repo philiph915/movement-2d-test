9bd2b1818055d9381e2e3cc83fe1591e610a0b3a
philiph915 <philip.h915@gmail.com>
2023-10-10 00:27:45 -0400

    Create using source file to establish namespaces to be globally used across the project

---
+++ Usings.cs
@@ -0,0 +1,4 @@
+// Use this file to ensure that all other cs source files will automatically be using these libraries
+global using Microsoft.Xna.Framework;
+global using Microsoft.Xna.Framework.Graphics;
+global using Microsoft.Xna.Framework.Input;
