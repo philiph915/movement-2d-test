2c00df66f93cf301324a9bfbbf9b5b3b944fa219
philiph915 <philip.h915@gmail.com>
2023-10-12 00:34:50 -0400

    add scene instantiation and scene draw call to game manager class

--- GameManager.cs
+++ GameManager.cs
@@ -4,6 +4,7 @@ public class GameManager
 {
 
     public readonly Player _player;
+    public readonly Scene1 _scene1;
 
     // Constructor
     public GameManager()
@@ -11,6 +12,9 @@ public class GameManager
         // Instantiate a player object with specified sprite and position
         _player = new(Globals.Content.Load<Texture2D>("hedgehog_transparent_small"),new(600, 600));
         _player.SetBounds(new(800,800),new(16,16)); 
+
+        // Instantiate Scene / Environment Objects
+        _scene1 = new();
     }
 
     // Update all the objects in the game
@@ -24,6 +28,7 @@ public class GameManager
     public void Draw()
     {
         Globals.SpriteBatch.Begin();
+        _scene1.Draw();
         _player.Draw();
 
         Globals.SpriteBatch.End();
