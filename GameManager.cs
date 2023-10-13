1889423bd7329321891ff982924314a601744659
philiph915 <philip.h915@gmail.com>
2023-10-13 00:07:09 -0400

    added camera logic; updated player bounds; added debugger logic to show dx and dy for camera translation calcs; updated camera to clamp to level boundaries

--- GameManager.cs
+++ GameManager.cs
@@ -5,16 +5,21 @@ public class GameManager
 
     public readonly Player _player;
     public readonly Scene1 _scene1;
+    private Matrix _translation;
+
+    public float delX;
+    public float delY;
 
     // Constructor
     public GameManager()
     {
-        // Instantiate a player object with specified sprite and position
-        _player = new(Globals.Content.Load<Texture2D>("hedgehog_transparent_small"),new(600, 600));
-        _player.SetBounds(new(800,800),new(16,16)); 
-
         // Instantiate Scene / Environment Objects
         _scene1 = new();
+
+        // Instantiate a player object with specified sprite and position
+        _player = new(Globals.Content.Load<Texture2D>("hedgehog_transparent_small"),new(600, 600));
+        _player.SetBounds(new(1920,1080),new(48,48)); 
+
     }
 
     // Update all the objects in the game
@@ -22,12 +27,31 @@ public class GameManager
     {
         InputManager.Update();
         _player.Update();
+        CalculateTranslation();
     }
+
+    private void CalculateTranslation()
+    {
+        var dx = (Globals.WindowSize.X / 2) - _player.Position.X;
+        dx = MathHelper.Clamp(dx, 1280-1920, 0);
+        var dy = (Globals.WindowSize.Y / 2) - _player.Position.Y;
+        dy = MathHelper.Clamp(dy, 720-1080+24, 0);
+        _translation = Matrix.CreateTranslation(dx,dy,0f);
+
+        // For debugging
+        delX = (float)dx;
+        delY = (float)dy;
+    }
+
+    //dx = MathHelper.Clamp(dx, -_map.MapSize.X + Globals.WindowSize.X + (_map.TileSize.X / 2), _map.TileSize.X / 2);
+     //   var dy = (Globals.WindowSize.Y / 2) - _hero.Position.Y;
+      //  dy = MathHelper.Clamp(dy, -_map.MapSize.Y + Globals.WindowSize.Y + (_map.TileSize.Y / 2), _map.TileSize.Y / 2);
+        
 
     // Draw all the sprites in the game
     public void Draw()
     {
-        Globals.SpriteBatch.Begin();
+        Globals.SpriteBatch.Begin(transformMatrix: _translation);
         _scene1.Draw();
         _player.Draw();
 
